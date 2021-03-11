b20def14c7bacde9133d72fece7d6b63cf6bd325
MovoLovo <48698410+MovoLovo@users.noreply.github.com>
2021-03-11 03:42:11 -0500

    Add scheduled sentiment calls

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from InvestopediaApi import ita
 from googlefinance import getQuotes
 from tinydb import TinyDB, Query
 import requests
-import json
 import helper
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 
 db = None
 cursor = None
+sched = BlockingScheduler()
 
 
 @helper.timer
@@ -29,7 +31,19 @@ def query():
     return cursor
 
 
-if __name__ == "__main__":
-    for val in get_data()[0]:
-        print(val["ticker"])
+def update_db(req):
+    for i, value in enumerate(req):
+        try:
+            arr = get_db().search(query().ticker == value)[0]["arr"]
+        except IndexError:
+            arr = []
 
+        arr.append((i, value["count_ticker"]))
+        get_db().upsert({"ticker": value["ticker"], "arr": arr}, query().name == value['ticker'])
+
+
+sched.add_job(update_db, "cron", [get_data()[0]], day_of_week='mon-fri', hour=20)
+
+
+if __name__ == "__main__":
+    sched.start()
---
+++ test.py
@@ -0,0 +1,5 @@
+temp = {"ticker": "GME", "count_ticker": 400}
+arr = []
+
+# print(temp["cheese"])
+print(arr[0])
